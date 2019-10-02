abfcb6489b265cecf3d9fb6b6385d564320329b1
digitalNomadRudolf <46172758+digitalNomadRudolf@users.noreply.github.com>
2019-10-02 12:50:53 +0100

    Update DiningInfo.js

--- src/components/dining-parts/DiningInfo.js
+++ src/components/dining-parts/DiningInfo.js
@@ -29,7 +29,7 @@ export default class DiningInfo extends Component {
         this.setState({
             name: '',
             email: '',
-            message: '';
+            message: ''
         });
     }
 
